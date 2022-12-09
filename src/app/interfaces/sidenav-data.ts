5d1aac9ec4923bf58beebfcf4ecf01b51c8574fe
Víctor Lavalle <vmlc1499@gmail.com>
2022-12-09 02:07:28 -0600

    💄 Fixed responsiveness on the login
    WIP

---
+++ src/app/interfaces/sidenav-data.ts
@@ -0,0 +1,27 @@
+export const navbarData = [
+    {
+        name: 'Inicio',
+        icon: 'bi bi-house',
+        routeLink: '/home'
+    },
+    {
+        name: 'Archivos',
+        icon: 'bi bi-search',
+        routeLink: '/search'
+    },
+    {
+        name: 'Estadísticas',
+        icon: 'bi bi-pie-chart',
+        routeLink: '/statistics'
+    },
+    {
+        name: 'Usuarios',
+        icon: 'bi bi-person-gear',
+        routeLink: '/management'
+    },
+    {
+        name: 'Perfil',
+        icon: 'bi bi-person-circle',
+        routeLink: '/profile'
+    }
+];
